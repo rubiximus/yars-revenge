94b80f065341dcbc51476418aa3a2dc2adc0b7e4
Andrew Otto <cpt.vimes@gmail.com>
2013-11-27 10:49:54 -0600

    moved utilities.py into animated_facing_sprite.py

--- animated_facing_sprite.py
+++ animated_facing_sprite.py
@@ -1,10 +1,10 @@
 
 
 import pygame
+from pygame import Surface, mask
 
 from asprite import ASprite
 from vector import *
-from utilities import *
 
 #dictionary from vector constants to list indeces
 DIRECTIONS = {NORTH:0, NORTHEAST:1, EAST:2, SOUTHEAST:3, SOUTH:4, SOUTHWEST:5, WEST:6, NORTHWEST:7}
@@ -73,4 +73,48 @@ class AnimatedFacingSprite(ASprite):
         return self.current_dir
         
         
-
+        
+def split_frames(filename, height, width, single_row=False):
+    """Splits a multi-sprite file into subsurfaces.
+    filename is the image file containing the sprites
+    height, width are the dimensions of the sprites
+    
+    returns a 2D list of Surfaces indexed [row][col]
+    returns normal list when image file is one row and single_row is set to True
+    
+    TODO: return matching list of Masks
+    """
+    
+    parent = pygame.image.load(filename).convert_alpha()
+    parent_width, parent_height = parent.get_size()
+    parent_rows = int(parent.get_height() / height)
+    parent_cols = int(parent.get_width() / width)
+    
+    if parent_rows == 1 and single_row:
+        return split_row(parent, parent_cols, 0, height, width)
+    
+    images = []
+    masks = []
+    
+    for row in range(parent_rows):
+        current_images, current_masks = split_row(parent, parent_cols, row, height, width)
+        images.append(current_images)
+        masks.append(current_masks)
+        
+    return images, masks
+    
+    
+def split_row(parent, parent_cols, row, height, width):
+    """split_frame helper function
+    splits a single row of source surface
+    """
+    
+    images = []
+    masks = []
+    
+    for col in range(parent_cols):
+        current_image = parent.subsurface((col*width, row*height, width, height))
+        images.append(current_image)
+        masks.append(mask.from_surface(current_image))
+        
+    return images, masks
